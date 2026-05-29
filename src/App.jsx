9a2de68c6290928cdb38cd7bf67e92543f68db22
Your Name <satheesh@bhoomaniholdings.com>
2026-05-29 14:42:22 +0530

    home changes'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import BasicInfo from './pages/Registration/BasicInfo/BasicInfo'
-import AppRoutes from './routes/AppRoutes'
+// import AppRoutes from './routes/AppRoutes'
+import AddressInfo from'./pages/Registration/AddressInfo/AddressInfo'
+import NomineeInfo from './pages/Registration/NomineeInfo/NomineeInfo'
+import   AboutUs from './pages/public/about/AboutUs'
 
 function App() {
   return (
    <>
-     <AppRoutes/>
+   <AboutUs/>
    </>
   )
 }
--- src/pages/Registration/NomineeInfo/NomineeInfo.jsx
+++ src/pages/Registration/NomineeInfo/NomineeInfo.jsx
@@ -1,90 +1,55 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 
-import { useNavigate } from "react-router-dom"
-import NomineeInfoConfiguration from "./NomineeInfoConfiguration.json"
-import {validateField} from './NomineeValidateField.js'
-import { validateForm } from "../../Registration/formvalidation.js";
-import { Input } from "../../../components/forms/Input/Input"
-import { Select } from "../../../components/forms/Select/Select"
+import { useNavigate } from 'react-router-dom'
+import NomineeInfoConfiguration from './NomineeInfoConfiguration.json'
+import { validateField } from './NomineeValidateField.js'
+import { validateForm } from '../../Registration/formvalidation.js'
+import { Input } from '../../../components/forms/Input/Input'
+import { Select } from '../../../components/forms/Select/Select'
 
-function NomineeInfo() {
+function NomineeInfo () {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate()
+  const [formData, setFormData] = useState(NomineeInfoConfiguration)
 
-    const [formData, setFormData] =
-        useState(NomineeInfoConfiguration)
+  const handleChange = event => {
+    validateField(event, formData, setFormData)
+  }
 
-    const handleChange = (event) => {
+  const handleSubmit = e => {
+    e.preventDefault()
 
-        validateField(
-            event,
-            formData,
-            setFormData
-        )
+    const { isFormvalid, updatedFormData } = validateForm(formData)
+
+    if (!isFormvalid) {
+      setFormData(updatedFormData)
+
+      return
     }
 
-    const handleSubmit = (e) => {
+    // alert("Submit Successfully")
 
-        e.preventDefault()
+    // Navigate to next page
+    navigate('/')
+  }
 
-        const {
-            isFormvalid,
-            updatedFormData
-        } = validateForm(formData)
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Nominee Information</h2>
 
-        if (!isFormvalid) {
+      {formData.map((item, index) => {
+        switch (item.type) {
+          case 'select':
+            return <Select key={index} {...item} handleChange={handleChange} />
 
-            setFormData(updatedFormData)
-
-            return
+          default:
+            return <Input key={index} {...item} handleChange={handleChange} />
         }
+      })}
 
-        // alert("Submit Successfully")
-
-        // Navigate to next page
-        navigate("/")
-    }
-
-    return (
-
-        <form onSubmit={handleSubmit}>
-
-            <h2>Nominee Information</h2>
-
-            {
-                formData.map((item, index) => {
-
-                    switch (item.type) {
-
-                        case "select":
-
-                            return (
-                                <Select
-                                    key={index}
-                                    {...item}
-                                    handleChange={handleChange}
-                                />
-                            )
-
-                        default:
-
-                            return (
-                                <Input
-                                    key={index}
-                                    {...item}
-                                    handleChange={handleChange}
-                                />
-                            )
-                    }
-                })
-            }
-
-            <button type="submit">
-                Submit
-            </button>
-
-        </form>
-    )
+      <button type='submit'>Submit</button>
+    </form>
+  )
 }
 
 export default NomineeInfo
