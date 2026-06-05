6497d92f356d52ec67c5e2e158f552979d045088
Your Name <satheesh@bhoomaniholdings.com>
2026-06-05 12:08:30 +0530

    changes made now

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import BasicInfo from './pages/Registration/BasicInfo/BasicInfo'
 import AppRoutes from './routes/AppRoutes'
 import AddressInfo from'./pages/Registration/AddressInfo/AddressInfo'
 import NomineeInfo from './pages/Registration/NomineeInfo/NomineeInfo'
-import   AboutUs from './pages/public/about/AboutUs'
-
-
-import AppRoutes from './routes/AppRoutes'
+import AboutUs from './pages/public/about/AboutUs'
 
 function App() {
   return (
